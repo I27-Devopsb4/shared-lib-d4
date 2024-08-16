5cfa7aab80ed038f7e4c077aa9debcebcf39ed1d
I27-Devopsb4 <boyapati.divya99@gmail.com>
2024-08-16 18:01:08 +0530

    added shared code

---
+++ src/com/divya/build/calculator.groovy
@@ -0,0 +1,15 @@
+package com.divya.builds;
+
+class Calculator {
+    def jenkins
+    Calculator (jenkins) {
+        this.jenkins = jenkins
+
+    }
+    def add(firstNumber, secondNumber) {
+        return firstNumber+secondNumber
+    }
+    def multiply(firstNumber, secondNumber) {
+        return firstNumber*secondNumber
+    }
+}
---
+++ vars/firstPipeline.groovy
@@ -0,0 +1,27 @@
+import com.divya.builds.Calculator;
+def call(Map pipelineparams) {
+    Calculator calculator = new Calculator(this)
+
+    pipeline {
+        agent any
+        environment {
+            APP_NAME = ${pipelineparams.appName}
+        }
+        stages {
+            stage ('AdditionStage') {
+                steps {
+                    script {
+                        echo "Printing sum of 2 numbers"
+                        println calculator.add(3,4)
+                        echo "******Microservice name is: ${APP_NAME}*********"
+                    }
+                }
+            }
+            stage ('SecondStage') {
+                steps {
+                    echo "Printing second stage"
+                }
+            }
+        }
+    }
+}
